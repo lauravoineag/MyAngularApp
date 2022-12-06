af74f37500284c340378d8d78a4fea49b04a0a76
Laura Voineag <laura_voineag@waters.com>
2022-12-06 15:08:49 +0000

    Add interpolation to bind the template expression to the class

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -7,5 +7,26 @@ import { Component } from "@angular/core";
 
 export class ProductListComponent{
   title:string = "Anime List"
-   
+  products: any[] = [
+    {
+      "productId": 2,
+      "productName": "Garden Cart",
+      "productCode": "GDN-0023",
+      "releaseDate": "March 18, 2021",
+      "description": "15 gallon capacity rolling garden cart",
+      "price": 32.99,
+      "starRating": 4.2,
+      "imageUrl": "assets/images/garden_cart.png"
+    },
+    {
+      "productId": 5,
+      "productName": "Hammer",
+      "productCode": "TBX-0048",
+      "releaseDate": "May 21, 2021",
+      "description": "Curved claw steel hammer",
+      "price": 8.9,
+      "starRating": 4.8,
+      "imageUrl": "assets/images/hammer.png"
+    }
+  ];
 }
