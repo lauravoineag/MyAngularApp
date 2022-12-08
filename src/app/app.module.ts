0f3b2adb4f7cff71ea487f3a59bb00f4d3889149
Laura Voineag <laura_voineag@waters.com>
2022-12-08 14:25:47 +0000

    Add pipe binding for currency and product code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from "./products/product-list.component";
@@ -13,6 +14,7 @@ import { ProductListComponent } from "./products/product-list.component";
     bootstrap: [AppComponent],
     imports: [
         BrowserModule,
+        FormsModule
     ]
 })
 export class AppModule { }
