629959e26af26e41992064c0d811491dc8ed301e
Laura Voineag <laura_voineag@waters.com>
2022-12-09 11:03:56 +0000

    Add interface as data type in product list component

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { IProduct } from "./iproduct";
 
 @Component({
   selector:"a-products",
@@ -11,7 +12,7 @@ export class ProductListComponent{
   imageMargin: number = 2;
   showImage: boolean = false;
   listFilter: string = "cart";
-  products: any[] = [
+  products: IProduct[] = [
     {
       "productId": 2,
       "productName": "Garden Cart",
